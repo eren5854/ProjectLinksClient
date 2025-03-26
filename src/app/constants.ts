f796f83769b39709ca1281d6bd331cb62d6e8c79
eren5854 <ihsanerendelibas@hotmail.com>
2025-03-26 05:50:09 +0300

    .

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,5 @@
-export const api:string = "https://localhost:7265/api/"
-export const image:string = "https://localhost:7265/Images/"
+// export const api:string = "https://localhost:7265/api/"
+// export const image:string = "https://localhost:7265/Images/"
+
+export const api:string = "https://projectlinksapi.erendelibas.xyz/api/"
+export const image:string = "https://projectlinksapi.erendelibas.xyz/Images/"
